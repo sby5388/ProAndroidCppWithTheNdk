8a2928ddf78f02ceea1e0c5e2518d21ebfbf3b82
sby5388 <44052699+sby5388@users.noreply.github.com>
2022-05-01 17:26:03 +0800

    add chap02 ndk-build
    添加ndk-build方式来构建hello-jni
    需要配置环境，添加ANDROID_USER_HOME和ANDROID_NDK_HOME到系统环境变量，一开始添加了ANDROID_SDK_HOME到系统环境变量中，显示冲突，提示使用ANDROID_USER_HOME替代ANDROID_SDK_HOME
    在macos 中添加的方式如下
    
    #add for android-sdk
    echo export ANDROID_USER_HOME=/Users/shenby07/Library/Android/sdk >> ~/.bash_profile
    echo export PATH=\$ANDROID_USER_HOME/tools:\$ANDROID_USER_HOME/platform-tools:\$PATH >> ~/.bash_profile
    
    #add for android-ndk
    echo export ANDROID_NDK_HOME=/Users/shenby07/Library/Android/sdk/ndk/24.0.8215888 >> ~/.bash_profile
    echo export PATH=\$ANDROID_NDK_HOME:\$PATH >> ~/.bash_profile
    
    之后 source ~/.bash_profile
    之后在module的根目录中执行 ndk-build  生成so，在libs中，并在build.gradle之中将libs设置为libs的目录
    jniLibs.srcDirs = ['libs']
    之后就可以正常编译，打包，安装，运行了

---
+++ chap02-ndk-build/src/com/shenby/pacwtn/chap02/ndkbuild/Chap02MainActivity.java
@@ -0,0 +1,23 @@
+package com.shenby.pacwtn.chap02.ndkbuild;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hellojni.HelloJni;
+import com.shenby.pacwtn.chap02.ndkbuild.databinding.ActivityChap02MainBinding;
+
+public class Chap02MainActivity extends AppCompatActivity {
+
+    private final HelloJni mHelloJni = new HelloJni();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final ActivityChap02MainBinding binding = ActivityChap02MainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.text.setText(mHelloJni.stringFromJNI());
+
+    }
+}
