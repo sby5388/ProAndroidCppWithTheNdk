bf8899ffa537770e97d8e4cf9eee7a8c39c8235c
sby5388 <44052699+sby5388@users.noreply.github.com>
2022-05-02 15:52:28 +0800

    chap04 swig 处理结构体 struct

---
+++ chap02-ndk-build/jni/my_struct.h
@@ -0,0 +1,12 @@
+//
+// Created by shenby07 on 2022/5/2.
+//
+
+#ifndef PROANDROIDCPPWITHTHENDK_MY_STRUCT_H
+#define PROANDROIDCPPWITHTHENDK_MY_STRUCT_H
+/** Point 结构体*/
+struct Point {
+    int x;
+    int y;
+};
+#endif //PROANDROIDCPPWITHTHENDK_MY_STRUCT_H
