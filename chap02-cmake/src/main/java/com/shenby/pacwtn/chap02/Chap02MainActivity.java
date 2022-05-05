d4e29bc4a3913d2e86d888cac5007561a90e5109
sby5388 <44052699+sby5388@users.noreply.github.com>
2022-05-06 00:41:46 +0800

    使用gradle+cmake来构建swig
    新增加一个swig模块时，编写完成一个swig.i之后,往swig.gradle添加一个SwigBean，然后点击构建，即可生成对应的native代码和JAVA代码；实际上使用命令行，会更加简单;
    在CMakeLists.txt中添加新的native代码文件参与构建，感觉不如ndk-build提供的生成脚本好用；

--- chap02-cmake/src/main/java/com/shenby/pacwtn/chap02/Chap02MainActivity.java
+++ chap02-cmake/src/main/java/com/shenby/pacwtn/chap02/Chap02MainActivity.java
@@ -3,21 +3,26 @@ package com.shenby.pacwtn.chap02;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.shenby.swig.Unix;
 import com.example.hellojni.HelloJni;
 import com.shenby.pacwtn.chap02.databinding.ActivityChap02MainBinding;
 
-public class Chap02MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class Chap02MainActivity extends AppCompatActivity {
+    public static final String TAG = "chap02-Cmake";
     private final HelloJni mHelloJni = new HelloJni();
+    private ActivityChap02MainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final ActivityChap02MainBinding binding = ActivityChap02MainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mBinding = ActivityChap02MainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
-        binding.text.setText(mHelloJni.stringFromJNI());
+        mBinding.text.setText(mHelloJni.stringFromJNI());
+
+        mBinding.textUid.setText(String.format(Locale.CHINESE, "uid:%d", Unix.getuid()));
 
     }
 }
